61b6652973bbca965d71dacbc0e359500ed8b0e3
bolu <bolu@bolupi.me>
2024-03-22 20:27:36 -0400

    fixed k_ary_tree and tests

--- src/algae/tree/k_ary_tree.hxx
+++ src/algae/tree/k_ary_tree.hxx
@@ -7,11 +7,13 @@
 namespace algae {
   namespace {
 
-    template<typename value, std::size_t k>
+    template<typename data_type, std::size_t k>
     struct node {
-      node<value, k> * parent;
-      std::array<node<value, k> *, k> child;
-      value v;
+      template<typename... as>
+      node(as &&... a) : data(std::forward<as>(a)...){};
+      node<data_type, k> * parent;
+      std::array<node<data_type, k> *, k> child;
+      data_type data;
     };
 
   }; // namespace
@@ -27,21 +29,39 @@ namespace algae {
       return r;
     };
 
+    void attach(node * u, std::size_t c, node * v) {
+      u->child[c] = v;
+      v->parent = u;
+    };
+
     template<typename... arguments>
-    node *& make_at(node *& u, arguments &&... args) {
-      u = std::allocator_traits<allocator>::allocate(1);
-
-      std::allocator_traits<allocator>::construct(
-        alloc,
-        u,
-        std::forward<arguments>(args)...
-      );
-
+    node * make_root(arguments &&... args) {
+      node * u = make_node(std::forward<arguments>(args)...);
+      root() = u;
       return u;
     };
 
+    template<typename... arguments>
+    node * make_child(node * u, std::size_t c, arguments &&... args) {
+      node * v = make_node(std::forward<arguments>(args)...);
+      attach(u, c, v);
+      return v;
+    };
+
     private:
-    allocator alloc;
+
+    using alloc_traits = std::allocator_traits<allocator>;
+    using alloc = typename alloc_traits::template rebind_traits<node>;
+
+    typename alloc::allocator_type a;
     node * r;
+
+    template<typename... arguments>
+    node * make_node(arguments &&... args) {
+      node * u = alloc::allocate(a, 1);
+      alloc::construct(a, u, std::forward<arguments>(args)...);
+      return u;
+    };
+
   };
 }; // namespace algae
